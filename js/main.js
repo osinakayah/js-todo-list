313d27f46a347f903286ff3a8a266cd642de553d
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-25 12:29:27 +0100

    Make Project Active

--- js/main.js
+++ js/main.js
@@ -205,3 +205,34 @@ const EventModule = function () {
     attachEventToProject
   }
 }();
+const UIModule = function(){
+  const makeProjectActive = (projectId) => {
+    document.querySelector(#projects-ul [data-project-id="${projectId}"]).classList.add("active")
+    document.querySelector('#project_task_relation_id').value = projectId
+  }
+  const removeProjectActive = (projectId) => {
+    document.querySelector(#projects-ul [data-project-id="${projectId}"]).classList.remove("active")
+  }
+  const renderProjectList = (projects) => {
+    const projectListTemplate = projects.reduce((projectRows, project, projectIndex) => {
+        return `${projectRows}
+            <li class="list-group-item project-item" data-project-id="${projectIndex}">
+                <a>
+                    <h6 data-project-id="${projectIndex}" class="message-list-title">${project.projectName}</h6>
+                </a>
+            </li>
+        `
+    }, '');
+
+    document.getElementById('projects-ul').innerHTML = projectListTemplate
+  }
+
+  return {
+    init: () => {
+        renderProjectList(ProjectModule.fetchProjects())
+    },
+    renderProjectList,
+    makeProjectActive,
+    removeProjectActive
+  }
+}();
